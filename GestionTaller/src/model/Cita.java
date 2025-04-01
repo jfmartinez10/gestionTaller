5f4a6fdf0d0a779dab4f2e50bb28d8d77f4ad2b2
carlos-morlans <cmorlansb@gmail.com>
2025-04-01 09:50:41 +0200

    model

--- GestionTaller/src/model/Cita.java
+++ GestionTaller/src/model/Cita.java
@@ -2,4 +2,14 @@ package model;
 
 public class Cita {
 
+    int id_cita;
+    String cliente;
+    int fecha;
+    String hora;
+    String descripcion;
+
+    public Cita (int id_cita, String cliente, int fecha, int hora, String descripcion){
+
+    }
+
 }
